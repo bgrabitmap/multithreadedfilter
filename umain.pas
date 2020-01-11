98765d7684f4083c44549f3a4d47a5e8e2628b0c
lainz <leandrodiazoe@gmail.com>
2020-01-11 10:41:13 -0300

    Update umain.lfm and umain.pas

--- umain.pas
+++ umain.pas
@@ -15,6 +15,7 @@ type
   TForm1 = class(TForm)
     BGRAVirtualScreen1: TBGRAVirtualScreen;
     procedure BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -33,7 +34,12 @@ implementation
 procedure TForm1.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
 begin
   Bitmap.GradientFill(0, 0, Bitmap.Width, Bitmap.Height, BGRA(0,255,0), BGRA(0,0,255), gtLinear, PointF(0,0), PointF(Bitmap.Height, 0), dmSet);
-  FilterGrayscale(Bitmap);
+  FilterGrayscale(Bitmap, TThread.ProcessorCount);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Self.Caption := IntToStr(TThread.ProcessorCount) + ' Threads';
 end;
 
 end.
